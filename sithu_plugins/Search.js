5588225016564b74455ced571e0926b2aa7fc458
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-05 19:51:14 +0530

    💛 ᴄʀᴇᴀᴛᴇ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ 💛

--- sithu_plugins/Search.js
+++ sithu_plugins/Search.js
@@ -16,6 +16,7 @@ const fetch = require('node-fetch')
    //---------------------------------------------------------------------------
    const { shazam } = require('../lib')
    let yts = require("secktor-pack");
+
    Module_Exports({
            kingcmd: "find",
            kingclass: "search",
@@ -80,7 +81,28 @@ catch (err) { return citel.reply("```Error While Fetching Snapshot```")}
         }
     )
 
+//-----------------------------------
 
+Module_Exports({
+    kingcmd: 'lyrics',
+    kingclass: "search",
+    infocmd: "lyrice search cmd",
+},
+  async (m,match) => {
+    match = match || m.reply_message.text;
+    if(!match) return await m.send(lang.BASE.TEXT);
+    const res = await fetchJson(config.BASE_URL+'api/lyrics?text='+match);
+    if(!res.status) return m.send(lang.BASE.ERROR.format("Not Found"));
+    if(!res.result) return m.send(lang.BASE.ERROR.format(",try again"));
+    const { thumb,lyrics,title,artist } = res.result, tbl= "```", tcl ="*_", tdl = "_*";
+        const msg = lang.LYRICS.RESPONCE.format(tcl+artist+tdl,tcl+title+tdl)+`\n\n${tbl}${lyrics}${tbl}`;
+        return await m.client.sendMessage(m.from, {
+            image: {url : thumb},
+            caption :msg
+        }, {
+            quoted: m
+        })
+});
 
     //---------------------------------------------------------------------------
 Module_Exports({
