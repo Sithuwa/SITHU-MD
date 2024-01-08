bc9f42a8a2c09ec25f631125621c8c73d6e2454e
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-08 21:53:05 +0530

    Update Downloaders.js

--- sithu_plugins/Downloaders.js
+++ sithu_plugins/Downloaders.js
@@ -159,11 +159,32 @@ async(sithu, person, memo) => {
 }
 
 )
+ //---------------------------------------------------------------------------
+    Module_Exports({
+        kingcmd: "fb",
+        infocmd: "Downloads video from fb.",
+        kingclass: "downloader",
+        react:"🎯️",
+    },
+async (message, match) => {
+match = match || message.reply_message.text;
+    if (!match)  return await message.reply("*_give me a url_*");
+    const urls = extractUrlsFromString(match);
+    if(!urls[0]) return await message.send("*_Give me a valid url_*");
+        const data = await getJson(`${config.BASE_URL}api/fb?url=${urls[0]}`);
+        const {status, result } = data;
+        if(!status) return await message.send("*Not Found*");
+        return await message.send(await getBuffer(result.hd), {}, 'video');
+});
+
+}
+)
     //---------------------------------------------------------------------------
     Module_Exports({
         kingcmd: "video",
         infocmd: "Downloads video from yt.",
         kingclass: "downloader",
+        react:"🎞️",
         kingpath: __filename,
         use: 'bella ciao',
     },
@@ -229,6 +250,7 @@ async(sithu, person, memo) => {
         kingcmd: "song",
         infocmd: "Downloads audio from youtube.",
         kingclass: "downloader",
+        react:"🧚‍♂️",
         kingpath: __filename,
         use: 'bella ciao',
     },
