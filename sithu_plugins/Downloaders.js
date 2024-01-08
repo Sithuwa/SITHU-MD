fc92a894e1dd94ea692f39eeeb34eca5934ccf66
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-08 22:23:01 +0530

    💛 ᴄʀᴇᴀᴛᴇ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ 💛

--- sithu_plugins/Downloaders.js
+++ sithu_plugins/Downloaders.js
@@ -159,23 +159,7 @@ async(sithu, person, memo) => {
 }
 
 )
- //---------------------------------------------------------------------------
-    Module_Exports({
-        kingcmd: "fb",
-        infocmd: "Downloads video from fb.",
-        kingclass: "downloader",
-        react:"🎯️",
-    },
-async (message, match) => {
-match = match || message.reply_message.text;
-    if (!match)  return await message.reply("*_give me a url_*");
-    const urls = extractUrlsFromString(match);
-    if(!urls[0]) return await message.send("*_Give me a valid url_*");
-        const data = await getJson(`${config.BASE_URL}api/fb?url=${urls[0]}`);
-        const {status, result } = data;
-        if(!status) return await message.send("*Not Found*");
-        return await message.send(await getBuffer(result.hd), {}, 'video');
-});
+
     //---------------------------------------------------------------------------
     Module_Exports({
         kingcmd: "video",
