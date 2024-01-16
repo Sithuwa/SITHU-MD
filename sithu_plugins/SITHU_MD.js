f5d6b677a3edbdae1b94401ae2e5d9cb06ba64cf
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-16 19:07:06 +0530

    Update SITHU_MD.js

--- sithu_plugins/SITHU_MD.js
+++ sithu_plugins/SITHU_MD.js
@@ -79,6 +79,10 @@ sɪᴛʜᴜ_ᴍᴅ.Module_Exports({
             filename: __filename
         },
         async(bot, person, text) => {
+                 await bot.sendPresenceUpdate('recording', person.chat);
+            await bot.sendMessage(person.chat, { audio: {url : 'https://github.com/Sithuwa/SITHUWA-MD/raw/main/media/bot.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: person, });
+        }
+    )
             const { commands } = require('../lib');
             if (text.split(" ")[0]) {
                 let Maher = [];
