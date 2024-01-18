fc2c3a6aff26790c9f71151098db05912b0e3136
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-18 14:34:13 +0530

    Update sithuwa.js

--- sithu_plugins/sithuwa.js
+++ sithu_plugins/sithuwa.js
@@ -2,11 +2,11 @@ const os = require('os');
 const { Sithu } = require('../lib')
 const axios = require('axios')
 const sɪᴛʜᴜ_speed = require('performance-now')
-const sithu = new Sithu();
+const client = new Sithu();
 
 //--------------------------------------------------------------------------------
 
-sithu.on('message', msg => {
+client.on('message', msg => {
     if (msg.body == '!ping') {
         msg.reply('pong');
     }
