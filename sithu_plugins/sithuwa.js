91537abdb7c1423f8136084fce8bc71742113784
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-18 14:26:04 +0530

    Update sithuwa.js

--- sithu_plugins/sithuwa.js
+++ sithu_plugins/sithuwa.js
@@ -1,22 +1,13 @@
 const os = require('os');
-const { tlang, botpic,Module_Exports,formatp } = require('../lib')
+const { Sithu } = require('../lib')
 const axios = require('axios')
 const sɪᴛʜᴜ_speed = require('performance-now')
+const sithu = new Sithu();
 
-//---------------------------------------------------------------------------
+//--------------------------------------------------------------------------------
 
-Module_Exports({
-
-            kingcmd: "gm",
-            shortcut:['goodmorning','ගුඩ්මෝනිම්'],
-            infocmd: "chat bot voice.",
-            kingclass: "voice chat",
-            react: "🥳",
-            filename: __filename,
-            use: '<faded-Alan walker.>',
-        },
-        async(Void, citel, text) => {
-              await Void.sendPresenceUpdate('recording', citel.chat);
-            await Void.sendMessage(citel.chat, { audio: {url : 'https://github.com/Sithuwa/SITHUWA-MD/raw/main/media/Gm.mp3',}, mimetype: 'audio/mpeg', ptt: true }, { quoted: citel, });
-        }
-    )
+sithu.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
