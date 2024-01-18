36e25d2927c03bd60d98727160b8c76d43d83cd5
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-18 18:46:32 +0530

    Add files via upload

---
+++ sithu_plugins/Auto_voice.js
@@ -0,0 +1,31 @@
+const { tlang,Module_Exports } = require('../lib')
+
+const Config = require('../config')
+
+const axios = require('axios')
+
+// Put here your url with modified words and urls
+
+const url = 'https://gist.github.com/Sithuwa/1f817ae8aefa4e73d16306a175f36906/raw'
+
+/**
+ Module_Exports({
+            pattern: "bgmnsew",
+            category: "owner",
+            use: '',
+        },
+**/ 
+
+Module_Exports({ on: "body" }, async (Void,citel,text)=> {
+
+if (Config.autoreaction === 'true' && citel.text.startsWith(prefix));
+
+  let { data } = await axios.get(url)
+
+  for (vr in data){
+
+ if((new RegExp(`\\b${vr}\\b`,'gi')).test(citel.text)) return Void.sendMessage(citel.chat,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:citel})   
+
+}
+
+})
