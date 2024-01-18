8973ea6735002d77a67b2d071e5252c033e504dd
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-18 23:12:12 +0530

    Update Find.js

--- sithu_plugins/Find.js
+++ sithu_plugins/Find.js
@@ -7,3 +7,50 @@
 //                                                                                                      //
 //══════════════════════════════════════════════════════════════════════════════════════════════════════//
 const _0x5734d5=_0x3b0a;(function(_0x28a45f,_0x374b45){const _0x4e0910=_0x3b0a,_0x93384e=_0x28a45f();while(!![]){try{const _0x200375=-parseInt(_0x4e0910(0x165))/0x1+-parseInt(_0x4e0910(0x16a))/0x2*(-parseInt(_0x4e0910(0x194))/0x3)+-parseInt(_0x4e0910(0x171))/0x4+-parseInt(_0x4e0910(0x176))/0x5*(-parseInt(_0x4e0910(0x167))/0x6)+parseInt(_0x4e0910(0x178))/0x7+parseInt(_0x4e0910(0x177))/0x8*(-parseInt(_0x4e0910(0x16b))/0x9)+parseInt(_0x4e0910(0x184))/0xa;if(_0x200375===_0x374b45)break;else _0x93384e['push'](_0x93384e['shift']());}catch(_0x411e4f){_0x93384e['push'](_0x93384e['shift']());}}}(_0x5bb3,0x8f260));function _0x3b0a(_0xd92da9,_0xafd606){const _0x5bb3d2=_0x5bb3();return _0x3b0a=function(_0x3b0a43,_0x1aa3a3){_0x3b0a43=_0x3b0a43-0x165;let _0x350041=_0x5bb3d2[_0x3b0a43];return _0x350041;},_0x3b0a(_0xd92da9,_0xafd606);}const {tlang,shazam,prefix,Module_Exports}=require(_0x5734d5(0x195)),axios=require(_0x5734d5(0x174));let yts=require(_0x5734d5(0x17c));function _0x5bb3(){const _0x31427b=['1200927WzAFPU','artists','\x20\x0a*Download\x20Url\x20:*\x20','ytmp4\x20','secktor-pack','\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Release\x20:*\x20','findvideo','Finds\x20info\x20about\x20song','album','Send/Reply\x20Image\x20','quoted','Download\x20Video','9402660KRsXxk','find\x20audio','thumbnail','url','වීඩියෝව\x20සොයන්න','*Reply\x20a\x20part\x20of\x20the\x20video*','\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Album\x20:*\x20','ගීතය\x20සොයන්න','ytmp3\x20','\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Artist\x20:*\x20','findaudio','release_date','test','find\x20mp3','reply','\x0a\x20\x20\x0a*Download\x20Url\x20:*\x20','9EbCgcX','../lib','*🌍\x20Loading\x20Information.....*','61547LNzuGE','Download\x20Song','2508nWycCR','sendMessage','find\x20video','488374nDQsjr','411363QyIwKz','chat','download','status','shaxam','mtype','629464VxWwoT','footer','title','axios','*Title\x20:*\x20','145HamyAr','184DGtlYQ'];_0x5bb3=function(){return _0x31427b;};return _0x5bb3();}Module_Exports({'kingcmd':_0x5734d5(0x18e),'shortcut':[_0x5734d5(0x191),_0x5734d5(0x185),'fa',_0x5734d5(0x18b)],'kingclass':'search','infocmd':_0x5734d5(0x17f)},async(_0xdce9ab,_0x5585f9,_0x849b92)=>{const _0x5f23e4=_0x5734d5;let _0xcb679f=_0x5585f9[_0x5f23e4(0x182)][_0x5f23e4(0x170)];if(!_0x5585f9[_0x5f23e4(0x182)])return _0x5585f9[_0x5f23e4(0x192)](_0x5f23e4(0x181)+prefix+_0x5f23e4(0x16f));if(!/audio/[_0x5f23e4(0x190)](_0xcb679f))return _0x5585f9[_0x5f23e4(0x192)]('*reply\x20a\x20part\x20of\x20the\x20audio*');let _0x4422ca=await _0x5585f9[_0x5f23e4(0x182)][_0x5f23e4(0x16d)]();_0x5585f9[_0x5f23e4(0x192)]('*🌍\x20Loading\x20Information.....*');let _0xd68861=await shazam(_0x4422ca);if(!_0xd68861['status'])return _0x5585f9[_0x5f23e4(0x192)](_0xd68861);let _0x2432c9=_0x5f23e4(0x175)+_0xd68861['title']+_0x5f23e4(0x18d)+_0xd68861['artists']+_0x5f23e4(0x18a)+_0xd68861[_0x5f23e4(0x180)]+_0x5f23e4(0x17d)+_0xd68861[_0x5f23e4(0x18f)],_0x1303f4=await yts(_0xd68861[_0x5f23e4(0x173)]),_0x3010a2=_0x1303f4['videos'][0x0],_0x6f87ea=[{'buttonId':prefix+_0x5f23e4(0x17b)+_0x3010a2['url'],'buttonText':{'displayText':_0x5f23e4(0x183)},'type':0x1},{'buttonId':prefix+_0x5f23e4(0x18c)+_0x3010a2[_0x5f23e4(0x187)],'buttonText':{'displayText':_0x5f23e4(0x166)},'type':0x1}],_0x461671={'image':{'url':_0x3010a2[_0x5f23e4(0x186)]},'caption':_0x5f23e4(0x175)+_0xd68861[_0x5f23e4(0x173)]+_0x5f23e4(0x18d)+_0xd68861[_0x5f23e4(0x179)]+_0x5f23e4(0x18a)+_0xd68861[_0x5f23e4(0x180)]+_0x5f23e4(0x17d)+_0xd68861[_0x5f23e4(0x18f)]+_0x5f23e4(0x17a)+_0x3010a2[_0x5f23e4(0x187)]+'\x0a\x0a*ᴄʀᴇᴀᴛᴇ\x20ʙʏ\x20ꜱɪᴛʜᴜ-ᴍᴅ*\x0a','footer':tlang()['footer'],'headerType':0x4};await _0xdce9ab['sendMessage'](_0x5585f9[_0x5f23e4(0x16c)],_0x461671,{'quoted':_0x5585f9});}),Module_Exports({'kingcmd':_0x5734d5(0x17e),'shortcut':['find\x20mp4',_0x5734d5(0x169),'fv',_0x5734d5(0x188)],'kingclass':'search','infocmd':_0x5734d5(0x17f)},async(_0x47410c,_0x70ba5b,_0x4357d9)=>{const _0x4239e6=_0x5734d5;let _0x36e4c9=_0x70ba5b[_0x4239e6(0x182)][_0x4239e6(0x170)];if(!_0x70ba5b[_0x4239e6(0x182)])return _0x70ba5b[_0x4239e6(0x192)](_0x4239e6(0x181)+prefix+'shaxam');if(!/video/[_0x4239e6(0x190)](_0x36e4c9))return _0x70ba5b[_0x4239e6(0x192)](_0x4239e6(0x189));let _0x1c765d=await _0x70ba5b[_0x4239e6(0x182)][_0x4239e6(0x16d)]();_0x70ba5b[_0x4239e6(0x192)](_0x4239e6(0x196));let _0x31e07a=await shazam(_0x1c765d);if(!_0x31e07a[_0x4239e6(0x16e)])return _0x70ba5b[_0x4239e6(0x192)](_0x31e07a);let _0xca242d=_0x4239e6(0x175)+_0x31e07a[_0x4239e6(0x173)]+'\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Artist\x20:*\x20'+_0x31e07a[_0x4239e6(0x179)]+_0x4239e6(0x18a)+_0x31e07a[_0x4239e6(0x180)]+_0x4239e6(0x17d)+_0x31e07a['release_date'],_0x44c260=await yts(_0x31e07a['title']),_0x24d016=_0x44c260['videos'][0x0],_0x37cbd5=[{'buttonId':prefix+_0x4239e6(0x17b)+_0x24d016['url'],'buttonText':{'displayText':_0x4239e6(0x183)},'type':0x1},{'buttonId':prefix+_0x4239e6(0x18c)+_0x24d016[_0x4239e6(0x187)],'buttonText':{'displayText':_0x4239e6(0x166)},'type':0x1}],_0x25fb44={'image':{'url':_0x24d016[_0x4239e6(0x186)]},'caption':_0x4239e6(0x175)+_0x31e07a['title']+'\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Artist\x20:*\x20'+_0x31e07a[_0x4239e6(0x179)]+'\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Album\x20:*\x20'+_0x31e07a[_0x4239e6(0x180)]+'\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0a*Release\x20:*\x20'+_0x31e07a[_0x4239e6(0x18f)]+_0x4239e6(0x193)+_0x24d016[_0x4239e6(0x187)]+'\x0a\x0a*ᴄʀᴇᴀᴛᴇ\x20ʙʏ\x20ꜱɪᴛʜᴜ-ᴍᴅ*\x0a','footer':tlang()[_0x4239e6(0x172)],'headerType':0x4};await _0x47410c[_0x4239e6(0x168)](_0x70ba5b[_0x4239e6(0x16c)],_0x25fb44,{'quoted':_0x70ba5b});});
+
+ 
+ Module_Exports({
+
+        kingcmd: "truecall",
+
+        shortcut: ["num","findnum","tc","trucall"],
+
+        kingclass: "search",
+
+        infocmd: "Finds info number"
+
+    },
+    
+  async (message, match) => {
+	if (match.match(/login/gi)) {
+		match = match.replace(/login/gi, '');
+		if (!match) return await message.send('_give me a number to send otp_');
+		const msg = await truecaller.set(match);
+		if (msg === true) return await message.send(`_successfully send otp to this ${match} number_\n_use *true otp* <key> to login_`);
+		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+	} else if (match.match(/logout/gi)) {
+		await truecaller.logout(match);
+		return await message.send(`_successfull_`);
+	} else if (match.match(/otp/gi)) {
+		match = match.replace(/otp/gi, '');
+		if (!match) return await message.send('_please provide an otp_');
+		const msg = await truecaller.otp(match);
+		if (msg === true) return await message.send(`_successfully Logined to Truecaller!!_`);
+		return await message.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+	}
+	let user = (message.mention.jid?.[0] || message.reply_message.mention.jid?.[0] || message.reply_message.sender || match).replace(/[^0-9]/g, '');
+	if (!user) return await message.send(`_reply to a user_`)
+	const res = await truecaller.search(user);
+	if (!res.status) return await message.send(res.message);
+	let msg = `╭─❮ truecaller ❯ ❏\n`//buntline n eromd edi
+	delete res.status;
+	for (const key in res) {
+		msg += `│ ${key.toLowerCase()}: ${res[key]}\n`;
+	}
+	msg += `╰─❏`;
+	return await message.send('```' + msg + '```', {quoted: message.data})
+});
+
+   }
+
+)
