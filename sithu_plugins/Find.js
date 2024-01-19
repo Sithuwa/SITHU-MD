585f6f85e37aa165e54283608de53751afbdad7b
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-19 12:04:20 +0530

    Update Find.js

--- sithu_plugins/Find.js
+++ sithu_plugins/Find.js
@@ -3,20 +3,15 @@ const { truecaller,isPublic,Module_Exports } = require('../lib')
 const axios = require('axios')
  
  Module_Exports({
-
         kingcmd: "truecall",
-
         shortcut: ["num","findnum","tc","trucall"],
-
         kingclass: "search",
-
-        infocmd: "Finds info number"
-
+        infocmd: "Finds info number",
+        
     },
-    
-  async (message, match, client) => {
-	if (!match && !message.quoted) return await message.reply('_Enter the number you want to search_');
-	const number = message.quoted ? message.quoted.sender : match.replace(/[^0-9]/g, '')
-	const result = await truecaller(number, message.user_id)
-	return await message.reply(result);
+  async (Void, citel, text) => {
+	if (!text && !citel.quoted) return await citel.reply('_Enter the number you want to search_');
+	const number = citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, '')
+	const result = await truecaller(number, citel.user_id)
+	return await citel.reply(result);
 })
