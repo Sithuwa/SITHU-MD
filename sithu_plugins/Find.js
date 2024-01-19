e1359d57a93adfbf9fc3434306a81c68d37526fb
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-19 12:47:41 +0530

    Update Find.js

--- sithu_plugins/Find.js
+++ sithu_plugins/Find.js
@@ -1,4 +1,4 @@
-const { truecaller,isPublic,Module_Exports } = require('../lib')
+const { truecaller, jidToNum, delTruecaller, getTruecaller, Module_Exports } = require('../lib')
 
 const axios = require('axios')
  
@@ -9,9 +9,35 @@ const axios = require('axios')
         infocmd: "Finds info number",
         
     },
-  async (Void, citel, text) => {
-	if (!text && !citel.quoted) return await citel.reply('_Enter the number you want to search_');
-	const number = citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, '')
-	let result = await truecaller(number, citel.user_id)
-	return await citel.reply(result);
-})
+  async (message, match) => {
+    match =
+      (message.mention[0] && jidToNum(message.mention[0])) ||
+      match ||
+      (message.reply_message && jidToNum(message.reply_message.jid))
+    if (!match) return await message.send(`*Example :* truecaller 919876543210`)
+    if (match === 'token') {
+      const token = await getTruecaller()
+      if (!token) return await message.send(`*Your not logined*`)
+      return await message.send(token, { quoted: message.quoted })
+    }
+    if (match === 'logout') {
+      await delTruecaller()
+      return await message.send(`Logged out from Truecaller.`)
+    }
+    const res = await truecaller.search(match)
+
+    if (res.message) {
+      return await message.send(res.message)
+    }
+    let msg = ''
+    if (res.name) msg += `*Name :* ${res.name}\n`
+    if (res.gender) msg += `*Gender :* ${res.gender}\n`
+    if (res.email) msg += `*Email :* ${res.email}\n`
+    msg += `*Type :* ${res.numberType}(${res.type})\n`
+    msg += `*Carrier :* ${res.carrier}\n`
+    msg += `*Number :* ${res.number}\n`
+    if (res.city) msg += `*City :* ${res.city}\n`
+    msg += `*DailingCode :* ${res.dialingCode}(${res.countryCode})`
+    await message.send(msg)
+  }
+)
