7e7d4d892d4979e8d9cdf163e0de6b567e1b8073
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-19 13:06:18 +0530

    Update Find.js

--- sithu_plugins/Find.js
+++ sithu_plugins/Find.js
@@ -1,4 +1,4 @@
-const { truecaller, jidToNum, delTruecaller, getTruecaller, Module_Exports } = require('../lib')
+const { truecaller,jidToNum,delTruecaller,getTruecaller,Module_Exports } = require('../lib')
 
 const axios = require('axios')
  
@@ -9,35 +9,9 @@ const axios = require('axios')
         infocmd: "Finds info number",
         
     },
-  async (citel, text) => {
-    text =
-      (citel.mention[0] && jidToNum(citel.mention[0])) ||
-      text ||
-      (citel.reply_message && jidToNum(citel.reply_message.jid))
-    if (!text) return await citel.send(`*Example :* truecaller 919876543210`)
-    if (text === 'token') {
-      const token = await getTruecaller()
-      if (!token) return await citel.send(`*Your not logined*`)
-      return await citel.send(token, { quoted: citel.quoted })
-    }
-    if (text === 'logout') {
-      await delTruecaller()
-      return await citel.send(`Logged out from Truecaller.`)
-    }
-    const res = await truecaller.search(text)
-
-    if (res.message) {
-      return await citel.send(res.message)
-    }
-    let msg = ''
-    if (res.name) msg += `*Name :* ${res.name}\n`
-    if (res.gender) msg += `*Gender :* ${res.gender}\n`
-    if (res.email) msg += `*Email :* ${res.email}\n`
-    msg += `*Type :* ${res.numberType}(${res.type})\n`
-    msg += `*Carrier :* ${res.carrier}\n`
-    msg += `*Number :* ${res.number}\n`
-    if (res.city) msg += `*City :* ${res.city}\n`
-    msg += `*DailingCode :* ${res.dialingCode}(${res.countryCode})`
-    await citel.send(msg)
-  }
-)
+  async (Void, citel, text) => {
+	if (!text && !citel.quoted) return await citel.reply('_Enter the number you want to search_');
+	const number = citel.quoted ? citel.quoted.sender : text.replace(/[^0-9]/g, '')
+	let result = await truecaller(number, citel.user_id)
+	return await citel.reply(result);
+})
