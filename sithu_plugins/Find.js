dce80b7ed4180a87ce08c8d168ac368eb1ae19e2
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-19 11:08:15 +0530

    Update Find.js

--- sithu_plugins/Find.js
+++ sithu_plugins/Find.js
@@ -14,32 +14,32 @@ const axios = require('axios')
 
     },
     
-  async (sithu, citel) => {
-	if (citel.citel(/login/gi)) {
-		citel = citel.replace(/login/gi, '');
-		if (!citel) return await sithu.send('_give me a number to send otp_');
-		const msg = await truecaller.set(citel);
-		if (msg === true) return await sithu.send(`_successfully send otp to this ${citel} number_\n_use *true otp* <key> to login_`);
-		return await sithu.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
-	} else if (citel.citel(/logout/gi)) {
-		await truecaller.logout(citel);
-		return await sithu.send(`_successfull_`);
-	} else if (citel.citel(/otp/gi)) {
-		citel = citel.replace(/otp/gi, '');
-		if (!citel) return await sithu.send('_please provide an otp_');
-		const msg = await truecaller.otp(citel);
-		if (msg === true) return await sithu.send(`_successfully Logined to Truecaller!!_`);
-		return await sithu.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+  async (citel, text) => {
+	if (text.text(/login/gi)) {
+		text = text.replace(/login/gi, '');
+		if (!text) return await citel.send('_give me a number to send otp_');
+		const msg = await truecaller.set(text);
+		if (msg === true) return await citel.send(`_successfully send otp to this ${text} number_\n_use *true otp* <key> to login_`);
+		return await citel.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
+	} else if (text.text(/logout/gi)) {
+		await truecaller.logout(text);
+		return await citel.send(`_successfull_`);
+	} else if (text.text(/otp/gi)) {
+		text = text.replace(/otp/gi, '');
+		if (!text) return await citel.send('_please provide an otp_');
+		const msg = await truecaller.otp(text);
+		if (msg === true) return await citel.send(`_successfully Logined to Truecaller!!_`);
+		return await citel.send(`*message:* _use *true logout* as first_\n*resone*: ${msg}`);
 	}
-	let user = (sithu.mention.jid?.[0] || sithu.reply_sithu.mention.jid?.[0] || sithu.reply_sithu.sender || citel).replace(/[^0-9]/g, '');
-	if (!user) return await sithu.send(`_reply to a user_`)
+	let user = (citel.mention.jid?.[0] || citel.reply_message.mention.jid?.[0] || citel.reply_message.sender || text).replace(/[^0-9]/g, '');
+	if (!user) return await citel.send(`_reply to a user_`)
 	const res = await truecaller.search(user);
-	if (!res.status) return await sithu.send(res.sithu);
+	if (!res.status) return await citel.send(res.message);
 	let msg = `╭─❮ truecaller ❯ ❏\n`//buntline n eromd edi
 	delete res.status;
 	for (const key in res) {
 		msg += `│ ${key.toLowerCase()}: ${res[key]}\n`;
 	}
 	msg += `╰─❏`;
-	return await sithu.send('```' + msg + '```', {quoted: sithu.data})
+	return await citel.send('```' + msg + '```', {quoted: citel.data})
 });
