799fe10658a338482579ef82eb9a2dded8d10627
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-22 11:35:37 +0530

    Update Misc.js

--- sithu_plugins/Misc.js
+++ sithu_plugins/Misc.js
@@ -45,3 +45,26 @@ Module_Exports({
  
  
       })
+
+
+Module_Exports({   
+       kingcmd: "nasa",      
+       react: "📃",   
+       infocmd: "",   
+       kingclass: "news",   
+       use: '.hirunews',   
+       filename: __filename   
+   },   
+   async(Void, citel) => {   
+   try{   
+   const nasa = await fetchJson(`https://vihangayt.me/details/nasa`);   
+  
+             const images = `${nasa.result.image}`   
+              const title = `${nasa.result.title}`  
+              const news = `${nasa.result.infocmd}`   
+  
+   await Void.sendMessage(citel.chat,  { image: { url: images }, caption: `📃ＮＡＳＡ ＢＹ ＳＩＴＨＵＷＡ\n\n*${ title }*\n\n _${news}._\n\n*📌 ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ...`}, { quoted: citel })   
+   }   
+   catch(e){   
+   console.log(e)   
+   }}) 
