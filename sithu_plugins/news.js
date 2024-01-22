701ba20d7b76c21bf0d304a4393f471bc9cc9189
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-22 16:57:00 +0530

    Update news.js

--- sithu_plugins/news.js
+++ sithu_plugins/news.js
@@ -4,8 +4,6 @@ const moment = require('moment-timezone')
  const axios = require('axios') 
  const fetch = require('node-fetch') 
  const Esana = require('@sl-code-lords/esana-news') 
- const Derana = require('@sl-code-lords/derana-news') 
- var api = new Derana() 
  var api = new Esana() 
 
 //---------------------------------------------------------------------------
@@ -30,26 +28,3 @@ Module_Exports({
                  await Void.sendMessage(citel.chat,{image:{url: img}, caption: cap})
  }) 
 
-//---------------------------------------------------------------------------
-
-Module_Exports({ 
-         kingcmd: "derana", 
-         kingclass: "news",
-         react: "📃",
-         infocmd: "Searches news", 
-         use: '<text>', 
-         filename: __filename, 
-     }, 
-     async(Void, citel) => { 
-        let res = await api.latest_id(); 
-        const nid = res.results.news_id; 
-        let news = await api.news(nid); 
-        const tt = news.results.TITLE; 
-        const dss = news.results.infocmdRIPTION; 
-        const ttime = news.results.PUBLISHED; 
-        const img = news.results.COVER; 
-        const cap = `📃𝗡𝗘𝗪𝗦 𝗕𝗬 𝗦𝗜𝗧𝗛𝗨-𝗠𝗗\n\n*☉───────────────*\n🏷️ ᴛɪᴛʟᴇ➨ ${tt}\n*☉───────────────*\n 🕒 ᴛɪᴍᴇ➨ ${ttime}\n*☉───────────────* \n 📑 ᴅᴇꜱᴄʀɪᴘᴛɪᴏɴ➨ ${dss}\n*☉───────────────* \n\n ▶.ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ.◀`; 
-                 await Void.sendMessage(citel.chat,{image:{url: img}, caption: cap})
- }) 
-
-//---------------------------------------------------------------------------
