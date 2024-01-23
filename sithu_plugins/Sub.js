4bdb8099b328699606c163b374e3984be604a59e
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-23 08:45:21 +0530

    Update Sub.js

--- sithu_plugins/Sub.js
+++ sithu_plugins/Sub.js
@@ -1,5 +1,13 @@
-const { getBuffer, getGroupAdmins, Module_Exports, sithu_plugins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
-const {subsearch , subdl }  = require('@sl-code-lords/si-subdl')
+const { tlang, ringtone, Module_Exports,fetchJson, sleep, botpic,ffmpeg, getBuffer, pinterest, prefix, Config } = require('../lib')
+const { mediafire } = require("../lib/mediafire.js");
+const googleTTS = require("google-tts-api");
+const ytdl = require('ytdl-secktor')
+const fs = require('fs-extra')
+var videotime = 60000 // 1000 min
+var dlsize = 10000 // 1000mb
+
+//---------------------------------------------------------------------------
+
 Module_Exports({
     kingcmd: "slsub",
     react: "📃",
@@ -8,50 +16,51 @@ Module_Exports({
     use: '.slsub',
     filename: __filename
 },
-async(conn, mek, m,{from, l, quoted, body, isCmd, sithu_plugins, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isCreator ,isDev, isAdmins, reply}) => {
-try{
-if (!q) return reply("❗ *Please enter movie name to download Subtitles*")
-const duka = await subsearch(q)
-const latest = await subdl(duka.results[0].link)
-const maru =`*SITHU-MD SINHALA SUB DOWNLOADER*
+async(Void, citel, text) => {
+   if (!text) return citel.reply("❗ *Please enter movie name to download Subtitles*")
+   let search = await subsearch(text)
+   let anu = await subdl(search.results[0].link)
+   let caption =`*SITHU-MD SINHALA SUB DOWNLOADER*
 
 📊 *Movie Title - ${latest.results.title}*
 
 🔒 Creator - ${latest.results.creater}
 
-🖇️ _Link_ - ${duka.results[0].link}
+🖇️ _Link_ - ${anu.results[0].link}
 
 `
-  await conn.sendMessage(from,{image:{url: latest.results.img },caption: maru + "*ꜱɪᴛʜᴜ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ*" },{quoted:mek })
-  await conn.sendMessage(from, { document : { url : latest.results.dl_link  }  ,caption: latest.results.title ,mimetype: 'application/zip', fileName: `${latest.results.title}.zip` }, { quoted: mek })
-} catch (e) {
-reply('🚫 *Error Accurated !!*\n\n' + e )
-l(e)
+  await Void.sendMessage(from,{image:{url: latest.results.img },caption: maru + "*ꜱɪᴛʜᴜ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ*" },{ quoted: citel })
+  await Void.sendMessage(from, { document : { url : latest.results.dl_link  }  ,caption: latest.results.title ,mimetype: 'application/zip', fileName: `${latest.results.title}.zip` }, { quoted: citel })
+} catch (error) citel.reply('🚫 *Error Accurated !!*');
 }
+return fs.unlinkSync(`./${randomName}`);     
 })
 
+  //---------------------------------------------------------------------------
+  
 Module_Exports({
-    kingcmd: "slsubfind",
+    kingcmd: "slsubsearch",
     react: "🔎",
     infocmd: "Search All Subtitles  from Web Site",
     kingclass: "search",
     use: '.technewsall',
     filename: __filename
 },
-async(conn, mek, m,{from, l, quoted, body, isCmd, sithu_plugins, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isCreator ,isDev, isAdmins, reply}) => {
-try{
-if (!q) return reply("❗ *Please enter movie name to Search Subtitles*")
-const vid = await subsearch(q)
-    let yt = '\n❍⚯────────────────────⚯❍\n        🌐  *𝚂𝙻 𝚂𝚄𝙱 𝚂𝙴𝙰𝚁𝙲𝙷 𝙻𝙸𝚂𝚃*  🌐\n ⚡ *ꜱɪᴛʜᴜ-ᴍᴅ ꜱʟ ꜱᴜʙᴛɪᴛʟᴇ ꜱᴇᴀʀᴄʜᴇʀ* ⚡\n❍⚯────────────────────⚯❍\n\n\n'
-    for (let i of vid.results ) {
-        yt += `📃 *${i.no} - ${i.title}*\n🔗 _Link : ${i.link}_ \n\n\n`
+async(Void, citel, text) => {
+   if (!text) return citel.reply("❗ *Please enter movie name to Search Subtitles*")
+   let search = await subsearch(text)
+   let yt = '\n❍⚯────────────────────⚯❍\n        🌐  *𝚂𝙻 𝚂𝚄𝙱 𝚂𝙴𝙰𝚁𝙲𝙷 𝙻𝙸𝚂𝚃*  🌐\n ⚡ *ꜱɪᴛʜᴜ-ᴍᴅ ꜱʟ ꜱᴜʙᴛɪᴛʟᴇ ꜱᴇᴀʀᴄʜᴇʀ* ⚡\n❍⚯────────────────────⚯❍\n\n\n'
+   for (let anu of vid.results ) {
+        yt += `📃 *${anu.no} - ${anu.title}*\n🔗 _Link : ${anu.url}_ \n\n\n`
     }
- await conn.sendMessage(from,{image:{url: "https://telegra.ph/file/d05c60fb40c1ab9f90600.jpg" },caption: yt + "*ꜱɪᴛʜᴜ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ*" },{quoted:mek })
-} catch (e) {
-reply('⛔ *Error accurated !!*\n\n' + e )
-l(e)
+ await Void.sendMessage(from,{image:{url: "https://telegra.ph/file/d05c60fb40c1ab9f90600.jpg" },caption: yt + "*ꜱɪᴛʜᴜ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ᴜꜱᴇʀ ʙᴏᴛ*\n*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ*" },{ quoted: citel })
+} catch (error) citel.reply('⛔ *Error accurated !!*');
 }
+return fs.unlinkSync(`./${randomName}`);     
+    
 })
+
+  //---------------------------------------------------------------------------
 
 Module_Exports({
     kingcmd: "subdlfromlink",
@@ -61,12 +70,11 @@ Module_Exports({
     use: '.subdlfromlink',
     filename: __filename
 },
-async(conn, mek, m,{from, l, quoted, body, isCmd, sithu_plugins, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isCreator ,isDev, isAdmins, reply}) => {
-try{
-if (!q) return reply("❗ Please enter movie Link to download Subtitles*")
-if(!q.includes('baiscope')) return reply('🚫 *Please enter Valid Movie url*')
- const latest = await subdl(q)
-const maru =`*SITHU-MD SL SUBTITLES DOWNLOADER*
+async(Void, citel, text) => {
+   if (!text) return citel.reply("❗ Please enter movie Link to download Subtitles*")
+   if(!text.includes('baiscope')) return reply('🚫 *Please enter Valid Movie url*')
+   let search = await subdl(search)
+   let caption =`*SITHU-MD SL SUBTITLES DOWNLOADER*
 
 📊 *Movie title - ${latest.results.title}*
 
@@ -76,10 +84,9 @@ const maru =`*SITHU-MD SL SUBTITLES DOWNLOADER*
 
 *ꜱɪᴛʜᴜ-ᴍᴅ*
 *ᴀʟʟ ʀɪɢʜᴛ ʀᴇꜱᴇʀᴠᴇᴅ - ʙʏ ꜱɪᴛʜᴜᴍ ᴋᴀʟʜᴀʀᴀ*`
- await conn.sendMessage(from , { text: maru }, { quoted: mek } )
-   await conn.sendMessage(from, { document : { url : latest.results.dl_link  }  ,caption: latest.results.title ,mimetype: 'application/zip', fileName: `${latest.results.title}.zip` }, { quoted: mek })
-} catch (e) {
-reply('🚫 *Error Accurated !!*\n\n' + e )
-l(e)
+ await Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
+   await Void.sendMessage(from, { document : { url : latest.results.dl_link  }  ,caption: latest.results.title ,mimetype: 'application/zip', fileName: `${latest.results.title}.zip` }, { quoted: citel })
+} catch (error) citel.reply('🚫 *Error Accurated !!*');
 }
+return fs.unlinkSync(`./${randomName}`);     
 })
