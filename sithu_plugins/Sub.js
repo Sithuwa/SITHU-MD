c6c46b7d53296dff87425d69036e84d02114a884
Sithum Kalhara <98222233+Sithuwa@users.noreply.github.com>
2024-01-23 08:30:35 +0530

    Update Sub.js

--- sithu_plugins/Sub.js
+++ sithu_plugins/Sub.js
@@ -1,6 +1,5 @@
-const name = require('../Setting')
-const { Module_Exports, sithu_plugins } = require('../sithu_plugins')
-const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const name = require('../setting')
+const { getBuffer, getGroupAdmins, Module_Exports, sithu_plugins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 const {subsearch , subdl }  = require('@sl-code-lords/si-subdl')
 Module_Exports({
     kingcmd: "slsub",
